0b66b108fd3552434fe58d048a73dd77ec05c589
RichardXD520 <richard.ortiz2101@alumnos.ubiobio.cl>
2025-06-01 02:24:07 -0400

    Update app.component.spec.ts

--- frontend/frontend/src/app/app.component.spec.ts
+++ frontend/frontend/src/app/app.component.spec.ts
@@ -1,14 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { AppModule } from './app.module';
 import { ComponentsModule } from 'src/app/shared/components/components.module';
 import { HomeComponent } from './home.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppModule]
+      imports: [
+        RouterTestingModule,
+        ComponentsModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
